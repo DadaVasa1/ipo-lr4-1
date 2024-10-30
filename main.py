d30aa1f11848661edd561d0e523a2fdaac20ca2d
Дарья <hzxz4610@gmail.com>
2024-10-30 18:09:49 +0300

    1 общее задание и 1 индивидуальное задание

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+number = int(input("Введите число: "))
+print("Четные числа", number)
+for i in range(number + 1):
+  if i % 2 == 0:
+    print(i)
+
+
+
+
+
+
 x = int(input("Введи значение x:"))  
 i = 1
 res = 1
